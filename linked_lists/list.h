20e3bc52c2c6a2d5d4b4e86d245efe72439a093f
Mario V <kbpmvb@hotmail.com>
2021-04-17 23:22:00 -0500

    Initial commit. Linked Lists.

---
+++ linked_lists/list.h
@@ -0,0 +1,32 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+
+typedef struct ListElement_ {
+    void *data;
+    struct ListElement_ *next;
+} ListElement;
+
+typedef struct List_ {
+    int size;
+    int (*match)(const void *key1, const void *key2);
+    void (*destroy) (void *data);
+
+    ListElement *head;
+    ListElement *tail;
+} List;
+
+void list_init(List *list, void (*destroy)(void *data));
+void list_destroy(List *list);
+int list_ins_next(List *list, ListElement *element, const void *data);
+int list_rem_next(List *list, ListElement *element, void **data);
+
+#define list_size(list) ((list)->size)
+#define list_head(list) ((list)->head)
+#define list_tail(list) ((list)->tail)
+#define list_is_head(list, element) ((element) == (list)->head ? 1 : 0)
+#define list_is_tail(element) ((element)->next == NULL ? 1 : 0)
+#define list_data(element) ((element)->data)
+#define list_next(element) ((element)->next)
+#endif
